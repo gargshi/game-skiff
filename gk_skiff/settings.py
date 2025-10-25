a02d7fc3d09988ed22eb594ecd2a732f73e15918
Shivam Garg <gargshivam34has@gmail.com>
2025-10-25 22:38:40 +0530

    mod settings.py static

--- gk_skiff/settings.py
+++ gk_skiff/settings.py
@@ -123,7 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'   # where collectstatic puts all files
+STATICFILES_DIRS = [
+    BASE_DIR / "static",                 # optional for your custom static files
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
