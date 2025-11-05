ad6cc60b0f65d320d9ffeaba6ac57bed686ca89f
Shivam Garg <gargshivam34has@gmail.com>
2025-11-05 20:59:57 +0530

    home-page-design-v1.2

--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,22 @@
-alert("hello world");
+const banner = document.querySelector('.banner');
+
+function loadBanner(){
+	// alert('Welcome to GK_SKIFF!');
+	master_color_change_dur_s=1;
+	master_animation_timeout=1000;
+	banner.style.opacity = '0';
+	setTimeout(() => {
+		banner.style.opacity = '1';
+		banner.style.transform = 'translateZ(0px)';
+	},master_animation_timeout);
+
+	setTimeout(() => {	
+		banner.style.color = 'var(--primary-color)';
+		banner.style.transform = 'translateY(-50px)';
+		banner.style.transition = 'transform 1s ease-in-out, opacity 1s ease-in-out, color '+master_color_change_dur_s+'s ease-in-out';		
+	},master_animation_timeout*2);
+
+}
+
+// setTimeout(loadBanner, 1000);
+loadBanner();
